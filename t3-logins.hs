27b9c4c4f1dfc5c925fc8b6d43e5d58ce03af76f
abribeiro <alsdrbuenoinf@gmail.com>
2015-04-13 02:23:10 -0300

    Revert "t3"
    This reverts commit abca8da3cfc5678986be0a9db1068c75797d2c3e.

--- t3-logins.hs
+++ t3-logins.hs
@@ -4,10 +4,5 @@ main :: IO ()
 main = do
     strcontent <- readFile "nomes.csv"
     let strlist = lines strcontent
-        user = login (strlist)
-        strnew = [x++"@inf.ufsm.br" | x<-user]
+        strnew = "str1":"str2":strlist
     writeFile "logins.csv" (unlines strnew)
-
-login :: [String] -> [String]
-login [] = []
-login n = [map toLower ((head x) : (reverse (takeWhile (/= ' ') (reverse x)))) | x <- n] 

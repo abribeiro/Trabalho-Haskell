abca8da3cfc5678986be0a9db1068c75797d2c3e
abribeiro <alsdrbuenoinf@gmail.com>
2015-04-13 02:22:25 -0300

    t3

--- t3-logins.hs
+++ t3-logins.hs
@@ -4,5 +4,10 @@ main :: IO ()
 main = do
     strcontent <- readFile "nomes.csv"
     let strlist = lines strcontent
-        strnew = "str1":"str2":strlist
+        user = login (strlist)
+        strnew = [x++"@inf.ufsm.br" | x<-user]
     writeFile "logins.csv" (unlines strnew)
+
+login :: [String] -> [String]
+login [] = []
+login n = [map toLower ((head x) : (reverse (takeWhile (/= ' ') (reverse x)))) | x <- n] 
